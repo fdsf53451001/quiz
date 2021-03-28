19c942b61196afd6fad29a4a3a39034baa30c238
fdsf53451001 <35889113+fdsf53451001@users.noreply.github.com>
2021-03-29 00:55:20 +0800

    quiz 1.3 JQuery change

--- quiz1.js
+++ quiz1.js
@@ -7,8 +7,15 @@ function change() {
    $("#good").html("怎麼那麼棒！！.");
 }
 function new_button(){
+    /*
     var btn = document.createElement("BUTTON"); 
     btn.innerHTML = "Change this document"; 
     document.body.appendChild(btn); 
     btn.addEventListener("click",change);
+    */
+    var change_btn = "<button id='change'>Change this document</button>";
+    $("body").append(change_btn);
+    $("#change").click(change);
 }
+
+$("#btn").click(new_button);
